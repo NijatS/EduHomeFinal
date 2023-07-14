0578b758aca416581ef8f34997486a534cd82b96
NijatS <nijatjs@code.edu.az>
2023-07-14 11:24:45 +0400

    AdminPanelAdded

---
+++ EduHome.App/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using EduHome.App.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduHome.App.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly EduHomeDbContext _context;
+
+        public HomeController(EduHomeDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
